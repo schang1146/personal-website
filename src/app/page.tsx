55375e32cdceb9f1108055ace9a202da06a09ae2
Sammy Chang <20074034+schang1146@users.noreply.github.com>
2023-12-29 16:45:35 -0800

    Add animated 3D box (#7)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+import dynamic from "next/dynamic";
+
+const DynamicThreeBox = dynamic(() => import("../components/three-box"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <>
+      <DynamicThreeBox></DynamicThreeBox>
       hello, world!
     </>
-  )
+  );
 }
