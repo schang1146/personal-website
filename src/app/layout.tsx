e8e0e6b157c8ff19ad420756196102be47db9817
schang1146 <20074034+schang1146@users.noreply.github.com>
2023-12-31 11:49:18 -0800

    Add prettierrc and lint current files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Sammy Chang',
-  description: 'Sammy Chang\'s Personal Website',
-}
+  title: "Sammy Chang",
+  description: "Sammy Chang's Personal Website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="min-h-screen grid grid-cols-1 grid-rows-2 px-24">{children}</main>
+        <main className="min-h-screen grid grid-cols-1 grid-rows-2 px-24">
+          {children}
+        </main>
       </body>
     </html>
-  )
+  );
 }
