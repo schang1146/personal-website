5544242c31132d1ff4c34b58b4fa61cdbfd59f3a
schang1146 <20074034+schang1146@users.noreply.github.com>
2024-01-01 15:36:27 -0800

    Add initials on the left section of the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between py-4">
-      <div></div>
+      <div>
+        <Link href="/">&#60;sc&#47;&#62;</Link>
+      </div>
       <ul>
         <li className="[&>*]:ml-4">
           <Link href="/">home</Link>
