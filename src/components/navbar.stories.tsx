c0948de5d07b61624f1d727fb29195590e9315bf
Sammy Chang <20074034+schang1146@users.noreply.github.com>
2024-01-08 18:19:31 -0800

    Add story for navbar (#28)

---
+++ src/components/navbar.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Navbar from "./navbar";
+
+const meta = {
+  title: "Navbar",
+  component: Navbar,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "padded",
+  },
+} satisfies Meta<typeof Navbar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LightModeLayout: Story = {
+  args: { themeToggle: "light", setThemeToggle: () => {} },
+};
