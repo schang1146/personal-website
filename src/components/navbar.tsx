ad957f6fbe1958b37c83a37435294a617abb6e11
schang1146 <20074034+schang1146@users.noreply.github.com>
2024-03-11 23:50:14 -0700

    Update navbar structure

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,26 +15,28 @@ export default function Navbar({
       <div>
         <Link href="/">&#60;sc&#47;&#62;</Link>
       </div>
-      <ul className="flex items-center gap-2">
+      <ul className="flex items-center gap-4">
         <li>
           <ThemeToggle
             themeToggle={themeToggle}
             setThemeToggle={setThemeToggle}
           ></ThemeToggle>
         </li>
-        <li>
-          <Link href="/">home</Link>
-        </li>
-        <li>
-          <Link
-            href="/projects"
-            className="pointer-events-none line-through text-gray-700"
-            aria-disabled="true"
-            tabIndex={-1}
-          >
-            projects
-          </Link>
-        </li>
+        <ul className="flex gap-4">
+          <li>
+            <Link href="/">home</Link>
+          </li>
+          <li>
+            <Link
+              href="/projects"
+              className="pointer-events-none line-through text-gray-700"
+              aria-disabled="true"
+              tabIndex={-1}
+            >
+              projects
+            </Link>
+          </li>
+        </ul>
       </ul>
     </nav>
   );
